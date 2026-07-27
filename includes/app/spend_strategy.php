1c84a88e7ad31b6c6d0e98b3cd03435f9af317f2
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-26 21:43:02 -0700

    Add Section 21 strategy module loader

---
+++ includes/app/spend_strategy.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/spend_strategy_demo.php';
+require_once __DIR__.'/spend_strategy_repository.php';
+require_once __DIR__.'/spend_strategy_engine.php';
+require_once __DIR__.'/spend_strategy_export.php';
+require_once __DIR__.'/spend_strategy_view_styles.php';
