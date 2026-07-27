8ca92e08d137e1d7479fa1e902f3ca451d041e05
bigriversocial74 <bigriversocial74@gmail.com>
2026-07-26 22:47:29 -0700

    Add Section 22 supplier portal module loader

---
+++ includes/app/supplier_portal.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+require_once __DIR__.'/supplier_portal_demo.php';
+require_once __DIR__.'/supplier_portal_repository.php';
+require_once __DIR__.'/supplier_portal_auth.php';
+require_once __DIR__.'/supplier_portal_service.php';
+require_once __DIR__.'/supplier_portal_export.php';
